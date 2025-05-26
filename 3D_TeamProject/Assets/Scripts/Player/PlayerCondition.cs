3a7c90798a0577f6ca81f73dbef4d10f8a7d048c
DH-C1 <cdh9633@gmail.com>
2025-05-26 16:19:54 +0900

    cs 추가
    Player 연관 cs 들과 UICondition cs 추가

---
+++ 3D_TeamProject/Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damage);
+}
+
+public class PlayerCondition : MonoBehaviour, IDamagable
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition hunger { get { return uiCondition.hunger; } }
+    Condition thirsty { get { return uiCondition.thirsty; }}
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (hunger.curValue < 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if (health.curValue < 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    public void Drink(float amount)
+    {
+        thirsty.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("GAME OVER");
+    }
+
+    public void TakePhysicalDamage(int damage)
+    {
+        health.Subtract(damage);
+        onTakeDamage?.Invoke();
+    }
+
+    public bool UseStamina(float amount)
+    {
+        if (stamina.curValue - amount < 0f)
+        {
+            return false;
+        }
+
+        stamina.Subtract(amount);
+        return true;
+    }
+}
