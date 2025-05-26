fab746b327f40533d3f634d38078a63a8754dd12
FKLA05 <brainkim2006@gmail.com>
2025-05-26 19:40:11 +0900

    폴더 정리

--- 3D_TeamProject/Assets/KJH_Work/KJH/PlayerNPCRay.cs
+++ 3D_TeamProject/Assets/KJH_Work/KJH/PlayerNPCRay.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRay : MonoBehaviour
+public class PlayerNPCRay : MonoBehaviour
 {
     void Start()
     {
