aaa9f0f8b878224225b89ea9a841c10c8d0f474d
DH-C1 <cdh9633@gmail.com>
2025-05-27 11:19:51 +0900

    cs 기능 추가 및 컨디션 HUD 연동
    허기와 목마름이 지속적으로 닳도록 기능 추가,
    
    허기 X: "천천히" 체력 소모
    목마름 X: "천천히" 체력 소모
    
    허기X, 목마름 X: "빠르게" 체력 소모
    
    점프 시: 스태미나 소모 <가만히 있을 시 스태미나 재생>

--- 3D_TeamProject/Assets/CDH_Work/Player/PlayerCondition.cs
+++ 3D_TeamProject/Assets/CDH_Work/Player/PlayerCondition.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using UnityEngine;
 
 public interface IDamagable
@@ -12,55 +12,62 @@ public class PlayerCondition : MonoBehaviour, IDamagable
 
     Condition health { get { return uiCondition.health; } }
     Condition hunger { get { return uiCondition.hunger; } }
-    Condition thirsty { get { return uiCondition.thirsty; }}
+    Condition thirsty { get { return uiCondition.thirsty; } }
     Condition stamina { get { return uiCondition.stamina; } }
 
     public float noHungerHealthDecay;
+    public float noThirstyHealthDecay;
     public event Action onTakeDamage;
 
     private void Update()
     {
-        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
-        stamina.Add(stamina.passiveValue * Time.deltaTime);
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);      //      매초 마다 배고픔 차감
+        thirsty.Subtract(thirsty.passiveValue * Time.deltaTime);    //      매초 마다 목마름 차감
+        stamina.Add(stamina.passiveValue * Time.deltaTime);         //      매초 마다 스태미나 회복
 
-        if (hunger.curValue < 0f)
+        if (hunger.curValue <= 0f)
         {
-            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);  //      배고픔이 0보다 낮을 때 체력 차감
         }
 
-        if (health.curValue < 0f)
+        if (thirsty.curValue <= 0f)
+        {
+            health.Subtract(noThirstyHealthDecay * Time.deltaTime); //      목마름이 0보다 낮을 때 체력 차감
+        }
+
+        if (health.curValue < 0f)       //      체력이 0보다 낮을 때 사망 함수 호출
         {
             Die();
         }
     }
 
-    public void Heal(float amount)
+    public void Heal(float amount)      //      체력 회복
     {
         health.Add(amount);
     }
 
-    public void Eat(float amount)
+    public void Eat(float amount)       //      배고픔 회복
     {
         hunger.Add(amount);
     }
 
-    public void Drink(float amount)
+    public void Drink(float amount)     //      목마름 회복
     {
         thirsty.Add(amount);
     }
 
-    public void Die()
+    public void Die()                   //      사망
     {
         Debug.Log("GAME OVER");
     }
 
-    public void TakePhysicalDamage(int damage)
+    public void TakePhysicalDamage(int damage)      //      피해입음
     {
         health.Subtract(damage);
         onTakeDamage?.Invoke();
     }
 
-    public bool UseStamina(float amount)
+    public bool UseStamina(float amount)            //      스태미나 소모
     {
         if (stamina.curValue - amount < 0f)
         {
