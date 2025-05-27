a44192d9408b4bd2e70347d71fc69948b3064839
FKLA05 <brainkim2006@gmail.com>
2025-05-27 14:33:26 +0900

    NPC 상호작용 개발중

---
+++ 3D_TeamProject/Assets/KJH_Work/KJH/NPCDialogue.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour
+{
+    [TextArea]
+    public string npcDialogue = "기본 대사입니다.";
+}
+
--- 3D_TeamProject/Assets/KJH_Work/KJH/PlayerNPCRay.cs
+++ 3D_TeamProject/Assets/KJH_Work/KJH/PlayerNPCRay.cs
@@ -1,20 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using TMPro;
 using UnityEngine;
 
 public class PlayerNPCRay : MonoBehaviour
 {
+    private float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float NPCrayDistance;
+    public LayerMask NPCLayer;
+    
+    private Camera cam;
+    public TextMeshProUGUI NPCText;
+    
     void Start()
     {
-        
+        cam = Camera.main;
     }
     void Update()
     {
-        Vector3 origin = transform.position;
-        Vector3 direction = transform.forward;
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
         
-        Physics.Raycast(origin, direction * 3f);//NPC 감지 ray
-        Debug.DrawRay(origin, direction * 3f, Color.red);
+            if (Physics.Raycast(ray, out hit, NPCrayDistance, NPCLayer)) // NPC 감지 RAY
+            {
+                NPCDialogue npc = hit.collider.GetComponent<NPCDialogue>();
+                if (npc != null)
+                {
+                    NPCText.text = npc.npcDialogue;
+                    NPCText.gameObject.SetActive(true);
+                    Debug.Log("NPC 감지됨: " + hit.collider.name);
+                    if (Input.GetKeyDown(KeyCode.F))
+                    {
+                       // var interactable = hit.collider.GetComponent<IInteractable>();
+                       // if (interactable != null)
+                        {
+                        //    interactable.Interact();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                NPCText.gameObject.SetActive(false);
+            }
+            Debug.DrawRay(ray.origin, ray.direction * NPCrayDistance, Color.red);
+        }
+        
         
     }
 }
