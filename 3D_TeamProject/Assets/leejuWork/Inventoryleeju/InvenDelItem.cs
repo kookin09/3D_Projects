42d5e338abab1059e0925c414490dc79d9bfc98e
ChunBae20 <drchunbae@gmail.com>
2025-05-30 07:01:52 +0900

    [feat]한개씩 사용하기 기능 추가
    지금 0이됐는데도 안사라지는 버그 있음QA요망

--- 3D_TeamProject/Assets/leejuWork/Inventoryleeju/InvenDelItem.cs
+++ 3D_TeamProject/Assets/leejuWork/Inventoryleeju/InvenDelItem.cs
@@ -16,4 +16,25 @@ public class InvenDelItem : MonoBehaviour
 
         inven.AssignUIPlace[slotIndex].SetUINullItemStack();
     }
+
+    public void ClickUseSlotItem(int slotIndex)
+    {
+        //현재 stack이 0보다 같거나 작아지면 아이템비우고
+        if (inven.AssignPlace[slotIndex].CurItemStack <= 0)
+        {
+            //실제로직
+            inven.AssignPlace[slotIndex].data = null;
+            inven.AssignPlace[slotIndex].CurItemStack = 0;
+            //ui에서 표시해줄로직
+            inven.AssignUIPlace[slotIndex].SetUINullItemStack();
+        }
+        //현재 아이템stack이 (0보다 크면)호출 될때마다 stack을 1씩 줄인다.
+        else
+        {
+            //실제 로직
+            inven.AssignPlace[slotIndex].CurItemStack -= 1;
+            //ui에서 받아올 로직
+            inven.AssignUIPlace[slotIndex].SetUIItemStack(inven.AssignPlace[slotIndex].data, inven.AssignPlace[slotIndex].CurItemStack);
+        }
+    }
 }
