b3c36b42636d5848018ee035935fd6f305b455ae
ChunBae20 <drchunbae@gmail.com>
2025-05-30 11:57:14 +0900

    [fix] 글씨 임시 추가
    [fix] 글씨 임시 추가

--- 3D_TeamProject/Assets/leejuWork/Inventoryleeju/InvenDelItem.cs
+++ 3D_TeamProject/Assets/leejuWork/Inventoryleeju/InvenDelItem.cs
@@ -19,6 +19,9 @@ public class InvenDelItem : MonoBehaviour
 
     public void ClickUseSlotItem(int slotIndex)
     {
+        //아니 이거 완벽한 로직 아니냐? 왜 0에서 한번 더눌러야 비워지지?
+        //누를때마다 1씩감소
+        inven.AssignPlace[slotIndex].CurItemStack -= 1;
         //현재 stack이 0보다 같거나 작아지면 아이템비우고
         if (inven.AssignPlace[slotIndex].CurItemStack <= 0)
         {
@@ -31,8 +34,7 @@ public class InvenDelItem : MonoBehaviour
         //현재 아이템stack이 (0보다 크면)호출 될때마다 stack을 1씩 줄인다.
         else
         {
-            //실제 로직
-            inven.AssignPlace[slotIndex].CurItemStack -= 1;
+            
             //ui에서 받아올 로직
             inven.AssignUIPlace[slotIndex].SetUIItemStack(inven.AssignPlace[slotIndex].data, inven.AssignPlace[slotIndex].CurItemStack);
         }
